032cfe4f81f52aee34bfec3b5a452fd731f0bb4e
c6h4clch3 <c6h4clch3@gmail.com>
2019-12-09 15:18:12 +0900

    fix: guard nullable value in

--- resources/assets/js/components/utils/ts/copyToClipboard.ts
+++ resources/assets/js/components/utils/ts/copyToClipboard.ts
@@ -1,16 +1,20 @@
 export function execCopy(string: string): boolean {
-  var temp = document.createElement('div');
+  var temp = document.createElement("div");
 
-  temp.appendChild(document.createElement('pre')).textContent = string;
+  temp.appendChild(document.createElement("pre")).textContent = string;
 
   var s = temp.style;
-  s.position = 'fixed';
-  s.left = '-100%';
+  s.position = "fixed";
+  s.left = "-100%";
 
   document.body.appendChild(temp);
-  document.getSelection().selectAllChildren(temp);
+  const selected = document.getSelection();
+  if (selected == undefined) {
+    return false;
+  }
+  selected.selectAllChildren(temp);
 
-  var result = document.execCommand('copy');
+  var result = document.execCommand("copy");
 
   document.body.removeChild(temp);
   // true なら実行できている falseなら失敗か対応していないか
